3d69b601eb55fd8e6c90db8101948cde1a8c32cc
DaeJin Jeong <devwithu@naver.com>
2017-08-28 10:31:05 +0900

    add echo_error rpc

--- JsonRpcExample/handlers/handler_info.js
+++ JsonRpcExample/handlers/handler_info.js
@@ -8,7 +8,7 @@ console.log('handler_info 파일 로딩됨.');
 
 var handler_info = [
 	{file:'./echo', method:'echo'}					// echo  
-	//,{file:'./echo_error', method:'echo_error'}		// echo_error       
+	,{file:'./echo_error', method:'echo_error'}		// echo_error       
 	//,{file:'./add', method:'add'}					// 더하기
 	//,{file:'./multiply', method:'multiply'}			// 곱하기
 	//,{file:'./listuser', method:'listuser'}			// 사용자 리스트
